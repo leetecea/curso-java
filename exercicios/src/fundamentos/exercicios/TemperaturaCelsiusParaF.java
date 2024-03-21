7a604b2ce9c3e6792a0933d999ffe5b27d9060ac
Letícia Costa <leticia_603295@trt5.jus.br>
2024-03-21 11:56:14 -0300

    realizando exercicios finais do mod. fundamentos

---
+++ exercicios/src/fundamentos/exercicios/TemperaturaCelsiusParaF.java
@@ -0,0 +1,21 @@
+package fundamentos.exercicios;
+
+import java.util.Scanner;
+
+public class TemperaturaCelsiusParaF {
+	
+	/** 1. Criar um programa que leia a temperatura em Celsius e converta para Fahrenheit. */
+	
+	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("Informe a temperatura em Celsius: ");
+		double celsius = entrada.nextDouble();
+		
+		double resultado = (celsius *  (9.0/5.0) + 32);
+		
+		System.out.printf("Temperatura em Fahrenheit é %.2f°F", resultado);
+		entrada.close();
+	}
+}
